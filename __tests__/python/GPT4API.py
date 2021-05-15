f33af44d1c30ecf06d30dece8a3c981abdab9e75
inococo <135534198+inococo@users.noreply.github.com>
2021-05-15 10:12:41 +0000

    Update coverage badge

--- __tests__/python/GPT4API.py
+++ __tests__/python/GPT4API.py
@@ -13,4 +13,18 @@ gpt4_endpoint = "https://api.openai.com/v1/engines/davinci-codex/completions"
 def send_image_and_text_to_gpt4(image_path, prompt):
     with open(image_path, "rb") as image_file:
         headers = CaseInsensitiveDict()
-        headers["Authorization"] = f"Bearer {api_key}
+        headers["Authorization"] = f"Bearer {api_key}"
+        files = {"image": image_file, "prompt": (None, prompt)}
+        response = requests.post(gpt4_endpoint, headers=headers, files=files)
+
+    if not response.ok:
+        raise Exception(f"GPT-4 API returned an error: {response.status_code}")
+
+    return response.json()
+
+if __name__ == "__main__":
+    image_path = "/Users/luhui/GitHub/cv/bags/Image__2023-04-16__10-02-31.png"
+    prompt = "Please count the plastic bags in the picture"
+
+    try:
+        result = se
