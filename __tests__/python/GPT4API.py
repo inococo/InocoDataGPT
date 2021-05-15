37399efc266bc28e5da34fa854cb516686d7efc5
inococo <135534198+inococo@users.noreply.github.com>
2021-05-15 07:48:05 +0000

    Updated Spanish translations.

--- __tests__/python/GPT4API.py
+++ __tests__/python/GPT4API.py
@@ -1,1 +1,16 @@
-# Note: it does not work. I got 404 
+# Note: it does not work. I got 404 error when running it.
+
+import os
+from dotenv import load_dotenv
+import requests
+from requests.structures import CaseInsensitiveDict
+
+load_dotenv()
+
+api_key = os.getenv("OPENAI_API_KEY")
+gpt4_endpoint = "https://api.openai.com/v1/engines/davinci-codex/completions"
+
+def send_image_and_text_to_gpt4(image_path, prompt):
+    with open(image_path, "rb") as image_file:
+        headers = CaseInsensitiveDict()
+        headers["Authorization"] = f"Bearer {api_key}
