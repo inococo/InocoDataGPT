d8dda9b2fbca96875428116ead115df3d6b0f832
inococo <135534198+inococo@users.noreply.github.com>
2021-06-02 16:05:02 +0000

    NPM update

--- __tests__/snowflake.test.ts
+++ __tests__/snowflake.test.ts
@@ -1,2 +1,13 @@
 // Note: The test cases here test both local and remote Snowflake APIs.
-// Please run "npm run test:custom" 
+// Please run "npm run test:custom" on another terminal before testing.
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import { querySnowflake } from "../src/server/snowflake";
+import { querySnowflakeAPI } from '../src/services/snowflake-service';
+import { getQueryOperatorStats as getQueryOperatorStatsExt } from "../src/utils/query-opstats";
+
+
+async function testQuerySnowflake() {
+  console.log('Snowflake: testing............................');
+  console.log("process.env.SNOWFLA
