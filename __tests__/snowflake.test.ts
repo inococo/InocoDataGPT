6d4eefc5c398b739a6a6267789c32248f56b8251
inococo <135534198+inococo@users.noreply.github.com>
2021-06-06 12:58:13 +0000

    systemd

--- __tests__/snowflake.test.ts
+++ __tests__/snowflake.test.ts
@@ -21,4 +21,12 @@ async function testQuerySnowflake() {
 async function getQueryOperatorStats() {
   // Step 1: Get the last query ID
   const queryIdQuery = 'SELECT last_query_id();';
-  const que
+  const queryIdResult = await querySnowflake(queryIdQuery);
+  const lastQueryId = queryIdResult[0]['LAST_QUERY_ID()'];
+  console.log("lastQueryId: ", lastQueryId);
+
+  // Step 2: Use the query ID to get the query operator stats
+  const operatorStatsQuery = `SELECT * FROM TABLE(get_query_operator_stats('${lastQueryId}'));`;
+  const operatorStatsResult = await querySnowflake(operatorStatsQuery);
+
+  console.log("get_query_operator_stats: ", operatorStatsResult);
