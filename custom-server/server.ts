4a0e65a933e4075b2aaeb3b33687cf4efda790cd
inococo <135534198+inococo@users.noreply.github.com>
2021-06-20 17:36:08 +0000

    add item

--- custom-server/server.ts
+++ custom-server/server.ts
@@ -1,4 +1,22 @@
 import 'dotenv/config';
 import express, { Request, Response, RequestHandler } from 'express';
 import { parse } from 'url';
-import n
+import next from 'next';
+import bodyParser from 'body-parser';
+import snowflakeHandler from '../src/server/snowflake';
+
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.use(bodyParser.json());
+
+  const snowflakeMiddleware: RequestHandler = async (req, res) => {
+    await snowflakeHandler(req, res);
+  };
+
+  server.u
