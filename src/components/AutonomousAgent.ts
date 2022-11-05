dded66c98fe9d13d71844baebea15cc8cc73f627
inococo <135534198+inococo@users.noreply.github.com>
2022-11-05 10:24:51 +0000

    Bumped version.

--- src/components/AutonomousAgent.ts
+++ src/components/AutonomousAgent.ts
@@ -2,4 +2,17 @@ import axios from "axios";
 import type { ModelSettings } from "../utils/types";
 import AgentService from "../services/agent-service";
 import {
-  DEFAULT_MAX
+  DEFAULT_MAX_LOOPS_CUSTOM_API_KEY,
+  DEFAULT_MAX_LOOPS_FREE,
+  DEFAULT_MAX_LOOPS_PAID,
+} from "../utils/constants";
+import type { Session } from "next-auth";
+import type { Message } from "../types/agentTypes";
+import { env } from "../env/client.mjs";
+import { v4 } from "uuid";
+import type { RequestBody } from "../utils/interfaces";
+
+const TIMEOUT_LONG = 1000;
+const TIMOUT_SHORT = 800;
+function sleep(milliseconds: number): Promise<void> {
+  return new Promise((r) => setTimeout(r, milliseconds)
