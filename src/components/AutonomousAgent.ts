2b3670d1791ee938f024334ef1921e0b8b545c5d
inococo <135534198+inococo@users.noreply.github.com>
2022-11-05 16:06:52 +0000

    Further improvements.

--- src/components/AutonomousAgent.ts
+++ src/components/AutonomousAgent.ts
@@ -15,4 +15,26 @@ import type { RequestBody } from "../utils/interfaces";
 const TIMEOUT_LONG = 1000;
 const TIMOUT_SHORT = 800;
 function sleep(milliseconds: number): Promise<void> {
-  return new Promise((r) => setTimeout(r, milliseconds)
+  return new Promise((r) => setTimeout(r, milliseconds));
+}
+
+class AutonomousAgent {
+  name: string;
+  goal: string;
+  tasks: string[] = [];
+  completedTasks: string[] = [];
+  modelSettings: ModelSettings;
+  isRunning = true;
+  renderMessage: (message: Message) => void;
+  shutdown: () => void;
+  numLoops = 0;
+  session?: Session;
+  _id: string;
+
+  constructor(
+    name: string,
+    goal: string,
+    renderMessage: (message: Message) => void,
+    shutdown: () => void,
+    modelSettings: ModelSettings,
+    session?:
