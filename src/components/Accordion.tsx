63a12bed746c8b28287b143cc0a636b71cb0957f
inococo <135534198+inococo@users.noreply.github.com>
2022-11-05 09:34:36 +0000

    Update coveralls badge

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -13,4 +13,15 @@ const Accordion = ({ child, name }: AccordionProps) => {
         <>
           <AccordionPrimitive.Button className="border:black delay-50 mb-1 flex w-full items-center justify-between rounded-xl bg-[#4a4a4a] px-3 py-2 text-sm tracking-wider outline-0 transition-all placeholder:text-white/20 hover:border-[#1E88E5]/40 hover:bg-[#6b6b6b] focus:border-[#1E88E5] focus-visible:ring sm:py-3 md:mb-3 md:text-lg">
             {name}
-            <FaChev
+            <FaChevronDown
+              className={`${open ? "rotate-180 transform" : ""} h-5 w-5`}
+            />
+          </AccordionPrimitive.Button>
+          <AccordionPrimitive.Panel>{child}</AccordionPrimitive.Panel>
+        </>
+      )}
+    </AccordionPrimitive>
+  );
+};
+
+export default Accordion;
---
+++ src/components/AutonomousAgent.ts
@@ -0,0 +1,5 @@
+import axios from "axios";
+import type { ModelSettings } from "../utils/types";
+import AgentService from "../services/agent-service";
+import {
+  DEFAULT_MAX
