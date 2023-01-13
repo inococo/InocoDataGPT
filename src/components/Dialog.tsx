655b1c0466280721002717e8940aeaad5ac95a96
inococo <135534198+inococo@users.noreply.github.com>
2023-01-13 14:06:55 +0000

    Reduce logging

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -16,4 +16,17 @@ export default function Dialog({
   close: () => void;
   footerButton?: React.ReactNode;
 }) {
-  const [ t ] = useTrans
+  const [ t ] = useTranslation()
+  if (!isShown) {
+    return <>{null}</>;
+  }
+
+  return (
+    <div className="fixed inset-0 z-40 flex items-center justify-center overflow-y-auto overflow-x-hidden bg-black/70 p-3 font-mono text-white outline-none transition-all">
+      <div
+        className="absolute bottom-0 left-0 right-0 top-0 "
+        onClick={close}
+      />
+      <div className="relative mx-auto my-6 w-auto max-w-3xl rounded-lg border-2 border-zinc-600">
+        {/*content*/}
+        <
