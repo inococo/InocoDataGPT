785968c8da0b0ab5030816672c3ed5753eabbcdd
inococo <135534198+inococo@users.noreply.github.com>
2023-02-04 12:57:46 +0000

    cifs: fix potential buffer overrun when composing a new options string

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -6,4 +6,27 @@ import {
   FaCloud,
   FaCog,
   FaDiscord,
-  FaG
+  FaGithub,
+  FaQuestionCircle,
+  FaRobot,
+  FaRocket,
+  FaSignInAlt,
+  FaSignOutAlt,
+  FaTwitter,
+  FaUser,
+} from "react-icons/fa";
+import clsx from "clsx";
+import { useAuth } from "../hooks/useAuth";
+import type { Session } from "next-auth";
+import { env } from "../env/client.mjs";
+import { api } from "../utils/api";
+import { useRouter } from "next/router";
+import { signIn } from "next-auth/react";
+
+const Drawer = ({
+  showHelp,
+  showSettings,
+  showDS,
+}: {
+  showHelp: () => void;
+  showSetti
