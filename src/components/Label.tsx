8f5ef5a55667e45265d2126e3903d38656e6df33
inococo <135534198+inococo@users.noreply.github.com>
2023-03-30 17:35:03 +0000

    Bump src/github.com/petergtz/bitsgo

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,1 +1,21 @@
 import React from "react";
+import Tooltip from "./Tooltip";
+import type { toolTipProperties } from "./types";
+
+interface LabelProps {
+  left?: React.ReactNode;
+  type?: string;
+  toolTipProperties?: toolTipProperties;
+}
+
+const Label = ({ type, left, toolTipProperties }: LabelProps) => {
+  const isTypeTextArea = () => {
+    return type === "textarea";
+  };
+
+  return (
+    <Tooltip
+      child={
+        <div
+          className={`center flex items-center rounded-xl rounded-r-none ${
+            type !== "range" ? "bo
