7249f641b47e456d400cf1fa7be11f737e8c25d8
inococo <135534198+inococo@users.noreply.github.com>
2023-03-31 19:20:01 +0000

    Fixed compiler warning.

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -18,4 +18,20 @@ const Label = ({ type, left, toolTipProperties }: LabelProps) => {
       child={
         <div
           className={`center flex items-center rounded-xl rounded-r-none ${
-            type !== "range" ? "bo
+            type !== "range" ? "border-r-0 border-white/10 md:border-[2px]" : ""
+          }  py-2 text-sm font-semibold tracking-wider transition-all sm:py-3 md:pl-3 md:text-lg
+          ${isTypeTextArea() ? "md:h-20" : ""}`}
+        >
+          {left}
+        </div>
+      }
+      style={{
+        container: `md:w-1/4`,
+      }}
+      sideOffset={0}
+      toolTipProperties={toolTipProperties}
+    ></Tooltip>
+  );
+};
+
+export default Label;
