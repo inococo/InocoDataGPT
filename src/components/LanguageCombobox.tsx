42d2899aa84d68cfc0dbe03be49862cd59c45095
inococo <135534198+inococo@users.noreply.github.com>
2023-04-10 13:16:11 +0000

    Improve help text

--- src/components/LanguageCombobox.tsx
+++ src/components/LanguageCombobox.tsx
@@ -1,1 +1,14 @@
-import React, { useState } fro
+import React, { useState } from "react";
+import { useTranslation } from "next-i18next";
+import type { Language } from "../utils/languages";
+import { ENGLISH, languages } from "../utils/languages";
+import { useRouter } from "next/router";
+import Input from "./Input";
+import { FaGlobe } from "react-icons/fa";
+
+const LanguageCombobox = () => {
+  const { i18n } = useTranslation();
+  const router = useRouter();
+  const [actualLanguage, setActualLanguage] = useState(
+    findLanguage(i18n.language)
+  );
