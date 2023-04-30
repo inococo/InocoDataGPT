285ae112dab5902a14076e48e490415fbf232b22
inococo <135534198+inococo@users.noreply.github.com>
2023-04-30 07:21:51 +0000

    Updated builds

--- src/components/pdf/MyDocument.tsx
+++ src/components/pdf/MyDocument.tsx
@@ -3,4 +3,32 @@ import { Document, Page, Text, StyleSheet, Font } from "@react-pdf/renderer";
 
 Font.register({
   family: "Roboto,SourceHanSansCN",
-  f
+  fonts: [
+    {
+      src: "/fonts/SourceHanSansCN-Regular.otf",
+    },
+    {
+      src: "/fonts/Roboto-Regular.ttf",
+    },
+  ],
+});
+
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: "column",
+    backgroundColor: "#FFFFFF",
+    padding: 40,
+  },
+  section: {
+    fontSize: 12,
+    fontFamily: "Roboto,SourceHanSansCN",
+    marginBottom: 20,
+    lineHeight: 1.5,
+  },
+});
+
+interface MyDocumentProps {
+  content: string;
+}
+
+const MyDocument: React.
