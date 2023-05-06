d1c7e6659b75e84926ee04a0934d9370c1fdf482
inococo <135534198+inococo@users.noreply.github.com>
2023-05-06 14:43:33 +0000

    timer: Fix jiffies wrap behavior of round_jiffies_common()

--- src/components/pdf/PDFButton.tsx
+++ src/components/pdf/PDFButton.tsx
@@ -3,4 +3,24 @@ import { FaFilePdf } from "react-icons/fa";
 import { pdf } from "@react-pdf/renderer";
 import React, { memo } from "react";
 import MyDocument from "./MyDocument";
-import type { Message } from "../../t
+import type { Message } from "../../types/agentTypes";
+
+import { useTranslation } from "react-i18next";
+
+const PDFButton = ({
+  messages,
+  name,
+}: {
+  messages: Message[];
+  name: string;
+}) => {
+  const content = getContent(messages);
+
+  const downloadPDF = async () => {
+    const blob = await pdf(<MyDocument content={content} />).toBlob();
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "my-document.pdf";
+    link.click();
+    URL.revokeOb
