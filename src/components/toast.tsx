e94a7447be7bd440e64f660af282e48cb54c160d
inococo <135534198+inococo@users.noreply.github.com>
2023-05-11 19:28:47 +0000

    Update JavaScript.md

--- src/components/toast.tsx
+++ src/components/toast.tsx
@@ -2,4 +2,24 @@ import * as ToastPrimitive from "@radix-ui/react-toast";
 import type { Dispatch, SetStateAction } from "react";
 import React from "react";
 import clsx from "clsx";
-import { useTran
+import { useTranslation  } from "next-i18next";
+
+type Props = {
+  model: [boolean, Dispatch<SetStateAction<boolean>>];
+  onAction?: () => void;
+  title: string;
+  description?: string;
+  className?: string;
+};
+
+const Toast = (props: Props) => {
+  const [ t ] = useTranslation();
+  const [open, setOpen] = props.model;
+
+  return (
+    <ToastPrimitive.Provider swipeDirection={"right"}>
+      <ToastPrimitive.Root
+        open={open}
+        onOpenChange={setOpen}
+        className={clsx(
+          "fixed inset-x-
