ef4bbb24e42b11d2ce3601e5c3112d1fa6e8e3a3
inococo <135534198+inococo@users.noreply.github.com>
2023-06-02 10:32:45 +0000

    release testing

--- src/env/client.mjs
+++ src/env/client.mjs
@@ -1,4 +1,20 @@
 // @ts-check
 import { clientEnv, clientSchema } from "./schema.mjs";
 
-const _
+const _clientEnv = clientSchema.safeParse(clientEnv);
+
+export const formatErrors = (
+  /** @type {import('zod').ZodFormattedError<Map<string,string>,string>} */
+  errors,
+) =>
+  Object.entries(errors)
+    .map(([name, value]) => {
+      if (value && "_errors" in value)
+        return `${name}: ${value._errors.join(", ")}\n`;
+    })
+    .filter(Boolean);
+
+if (!_clientEnv.success) {
+  console.error(
+    "❌ Invalid environment variables:\n",
+    ...forma
