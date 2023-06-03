45e5bfd890079b3dbf9822a078cc262e4a80fd9f
inococo <135534198+inococo@users.noreply.github.com>
2023-06-03 14:08:52 +0000

    for test

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,2 +1,19 @@
 import type { Session } from "next-auth";
-import { signIn, SignInRes
+import { signIn, SignInResponse, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { z } from "zod";
+
+const UUID_KEY = "uuid";
+
+type Provider = "google" | "github";
+
+interface Auth {
+  signIn: (provider?: Provider) => any;
+  signOut: () => any;
+  status: "authenticated" | "unauthenticated" | "loading";
+  session: Session | null;
+}
+
+export function useAuth(): Auth {
+  const { data: session, status
