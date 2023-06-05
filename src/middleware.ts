88f922272e5a712775224fc0c13008efb43d1f2f
inococo <135534198+inococo@users.noreply.github.com>
2023-06-05 16:15:35 +0000

    Небольшие правки

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,4 +3,20 @@ import { ipAddress } from "@vercel/edge";
 import { rateLimiter } from "./server/redis";
 
 export const config = {
-  // Only run the middleware on t
+  // Only run the middleware on the home route
+  matcher: "/api/agent/:path*",
+};
+
+function ipFallback(request: Request) {
+  const xff = request.headers.get("x-forwarded-for");
+  return xff ? (Array.isArray(xff) ? xff[0] : xff.split(",")[0]) : "127.0.0.1";
+}
+
+async function shouldRateLimit(request: NextRequest): Promise<boolean> {
+  const ip = ipAddress(request) || ipFallback(request) || "";
+  if (!ip) {
+    return false;
+  }
+
+  const { success } = await rateLimiter.limit(ip);
+  return !succe
