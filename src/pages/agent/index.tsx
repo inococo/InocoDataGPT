2738d953f963a4388fdb936652198578f20cc892
inococo <135534198+inococo@users.noreply.github.com>
2023-06-10 05:56:10 +0000

    update output

--- src/pages/agent/index.tsx
+++ src/pages/agent/index.tsx
@@ -1,2 +1,14 @@
 import { type NextPage } from "next";
-import DefaultLayout from "../../layout/default
+import DefaultLayout from "../../layout/default";
+import Button from "../../components/Button";
+
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import { api } from "../../utils/api";
+import ChatWindow from "../../components/ChatWindow";
+import type { Message } from "../../types/agentTypes";
+import Toast from "../../components/toast";
+import { FaTrash, FaShare, FaBackspace } from "react-icons/fa";
+import { env } from "../../env/client.mjs";
+
+import { useTranslatio
