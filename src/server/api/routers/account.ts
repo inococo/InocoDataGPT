ae47cbbd6faddc4fff637db5523fe12e30a5c149
inococo <135534198+inococo@users.noreply.github.com>
2023-06-25 13:46:44 +0000

    genirq: Avoid deadlock in spurious handling

--- src/server/api/routers/account.ts
+++ src/server/api/routers/account.ts
@@ -1,2 +1,14 @@
 import { createTRPCRouter, protectedProcedure } from "../trpc";
-import Str
+import Stripe from "stripe";
+import { env } from "../../../env/server.mjs";
+import { prisma } from "../../db";
+import { getCustomerId } from "../../../utils/stripe-utils";
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+const stripe = new Stripe(env.STRIPE_SECRET_KEY ?? "", {
+  apiVersion: "2022-11-15",
+});
+
+export const accountRouter = createTRPCRouter({
+  subscribe: protectedProcedure.mutation(async ({ ctx }) => {
+    const use
