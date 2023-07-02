a6785a25d666b8c210907a0e37d2803500ff3652
inococo <135534198+inococo@users.noreply.github.com>
2023-07-02 19:20:28 +0000

    Update groups.json

--- src/server/api/routers/agentRouter.ts
+++ src/server/api/routers/agentRouter.ts
@@ -2,4 +2,22 @@ import { z } from "zod";
 
 import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 import { prisma } from "../../db";
-import {
+import { messageParser } from "../../../types/agentTypes";
+
+const saveAgentParser = z.object({
+  name: z.string(),
+  goal: z.string(),
+  tasks: z.array(messageParser),
+});
+
+export const agentRouter = createTRPCRouter({
+  create: protectedProcedure
+    .input(saveAgentParser)
+    .mutation(async ({ input, ctx }) => {
+      const agent = await prisma.agent.create({
+        data: {
+          name: input.name,
+          goal: input.goal,
+          userId: ctx.session?.user?.id,
+        },
+      });
