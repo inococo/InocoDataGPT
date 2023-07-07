27f43997ca5350c97c37b7e84323642ebf6b4273
inococo <135534198+inococo@users.noreply.github.com>
2023-07-07 21:43:11 +0000

    white space

--- src/server/snowflake.ts
+++ src/server/snowflake.ts
@@ -1,1 +1,13 @@
-import * as snowflake from "snow
+import * as snowflake from "snowflake-sdk";
+import { SnowflakeError } from "snowflake-sdk";
+import { Request, Response } from 'express';
+
+
+const connectionConfig = {
+  account: process.env.SNOWFLAKE_ACCOUNT ?? '',
+  username: process.env.SNOWFLAKE_USERNAME ?? '',
+  password: process.env.SNOWFLAKE_PASSWORD ?? '',
+  region: process.env.SNOWFLAKE_REGION ?? '',
+  warehouse: process.env.SNOWFLAKE_WAREHOUSE ?? '',
+  database: process.env.SNOWFLAKE_DATABASE ?? '',
+  sche
