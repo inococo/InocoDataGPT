f1ffbe11efb69a3a9ffb7c90c2c3b6b8a66d5bcf
inococo <135534198+inococo@users.noreply.github.com>
2023-07-13 07:50:07 +0000

    fix cover

--- src/services/snowflake-service.ts
+++ src/services/snowflake-service.ts
@@ -6,4 +6,13 @@ export async function querySnowflakeAPI(sqlText: string): Promise<any> {
     const snowflakeResponse = await fetch('http://localhost:3001/api/snowflake', {
       method: 'POST',
       body: JSON.stringify({ sql: sqlText }),
-      headers: { 'Content
+      headers: { 'Content-Type': 'application/json' },
+    });
+    const data = await snowflakeResponse.json();
+    console.log("querySnowflakeAPI fetch finished.");
+    return data;
+  } catch (err) {
+    console.error('querySnowflakeAPI Error:', err);
+    throw err;
+  }
+}
