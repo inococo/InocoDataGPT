dd4e3b78fbce5c1b11f13d1e35a2eb24056ce33f
inococo <135534198+inococo@users.noreply.github.com>
2023-07-22 12:44:41 +0000

    corrected grammar

--- src/types/snowflake-sdk.d.ts
+++ src/types/snowflake-sdk.d.ts
@@ -1,1 +1,20 @@
-declare module 
+declare module 'snowflake-sdk' {
+  export interface Connection {
+    connect: (callback: (err: any, conn: Connection) => void) => void;
+    execute: (options: {
+      sqlText: string;
+      complete: (err: any, stmt: any, rows: any[]) => void;
+    }) => void;
+  }
+
+  export function createConnection(options: {
+    account: string;
+    username: string;
+    password: string;
+    region: string;
+    warehouse: string;
+    database: string;
+    schema: string;
+  }): Connection;
+
+  export
