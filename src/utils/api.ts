3ed6d60537e27ccba1f8c08a198a6fd7f80696a3
inococo <135534198+inococo@users.noreply.github.com>
2023-07-23 20:41:04 +0000

    kernel

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -6,4 +6,13 @@
  * We also create a few inference helpers for input and output types
  */
 import { httpBatchLink, loggerLink } from "@trpc/client";
-import { createT
+import { createTRPCNext } from "@trpc/next";
+import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
+import superjson from "superjson";
+
+import { type AppRouter } from "../server/api/root";
+
+const getBaseUrl = () => {
+  if (typeof window !== "undefined") return ""; // browser should use relative url
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use loca
