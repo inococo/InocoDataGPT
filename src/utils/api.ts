cdd485cf79fd7f318ed6a78cc2b633cbef778f7d
inococo <135534198+inococo@users.noreply.github.com>
2023-07-23 22:21:47 +0000

    Updated pod

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -15,4 +15,21 @@ import { type AppRouter } from "../server/api/root";
 const getBaseUrl = () => {
   if (typeof window !== "undefined") return ""; // browser should use relative url
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use loca
+  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+};
+
+/**
+ * A set of typesafe react-query hooks for your tRPC API
+ */
+export const api = createTRPCNext<AppRouter>({
+  config() {
+    return {
+      /**
+       * Transformer used for data de-serialization from the server
+       * @see https://trpc.io/docs/data-transformers
+       **/
+      transformer: superjson,
+
+      /**
+       * Links used to determine request flow from client to server
+       * @see https://trpc.
