c992b9583e3e58736ed4eba166fd9435d28b8694
inococo <135534198+inococo@users.noreply.github.com>
2023-08-07 08:43:38 +0000

    Add .DS_Store to gitignore

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -23,4 +23,17 @@ export const extractTasks = (
 ): string[] => {
   return extractArray(text)
     .filter(realTasksFilter)
-    .fil
+    .filter((task) => !(completedTasks || []).includes(task));
+};
+
+export const extractArray = (inputStr: string): string[] => {
+  // Match an outer array of strings (including nested arrays)
+  const regex = /(\[(?:\s*"(?:[^"\\]|\\.)*"\s*,?)+\s*\])/;
+  const match = inputStr.match(regex);
+
+  if (match && match[0]) {
+    try {
+      // Parse the matched string to get the array
+      return JSON.parse(match[0]) as string[];
+    } catch (error) {
+      console.error("Error parsing the matc
