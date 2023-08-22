f22a2a087a30375184d905ac951cd536ea56a483
inococo <135534198+inococo@users.noreply.github.com>
2023-08-22 22:31:15 +0000

    rm console.log

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,2 +1,18 @@
 /** @type {import("tailwindcss").Config} */
-const defaultTh
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      "xs": "300px",
+
+      "sm-h": { "raw": "(min-height: 700px)" },
+      "md-h": { "raw": "(min-height: 800px)" },
+      "lg-h": { "raw": "(min-height: 1000px)" },
+
+      ...defaultTheme.screens
+    },
+    extend: {
+      boxShadow: {
+        "3xl": "0 40px 70px -15px rgba(0, 0, 0, 0.40)" // Customize the shadow value according to your preferences.
